44f31a3b06dfbed1b83f389c8cab2a5d5bb20ef5
Tony Xiao <tonyx.ca@gmail.com>
2020-08-20 01:03:42 +0800

    Clean up unused files and requirements

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # plain-text-accounting
-Alka Plain Text Accounting
 
-
-converter/models.py is generated via quicktype.io
-
-https://app.quicktype.io/
+API to convert plain text accounting to JSON and back
--- archive/json_to_bean.py
+++
@@ -1,9 +0,0 @@
-from converter.models import ledger_from_dict
-from converter.utils import json_load_decimal, print_entries
-from converter.to_bean import entries_from_ledger
-
-ledger = ledger_from_dict(json_load_decimal(open("./pta.local.json", "r")))
-entries = entries_from_ledger(ledger)
-
-print_entries(list(entries))
-
