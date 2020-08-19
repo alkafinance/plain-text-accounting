8d87775c7d435ed658ef296589085f55a7180103
Tony Xiao <tonyx.ca@gmail.com>
2020-08-19 22:47:30 +0800

    Print beancount file directly into json rather to minimize amount of code in python

--- converter/utils.py
+++ converter/utils.py
@@ -1,8 +1,9 @@
 from typing import Optional, Any, List, TypeVar, Type, cast, Callable
 from beancount.core.data import Decimal, D, Entries
+from beancount.core import data as bean
 from beancount.core.display_context import DisplayContext
 from beancount.parser import printer
-from datetime import datetime
+from datetime import datetime, date
 
 import dateutil.parser
 import json
@@ -39,7 +40,12 @@ def json_load_decimal(fp) -> Any:
 
 
 def json_dumps_decimal(obj):
-    return simplejson.dumps(clean_nones(obj), use_decimal=True)
+    return simplejson.dumps(obj, use_decimal=True, default=date_to_json)
+
+
+def date_to_json(o: Any):
+    if isinstance(o, (date, datetime)):
+        return o.isoformat()
 
 
 def clean_nones(value):
@@ -51,13 +57,10 @@ def clean_nones(value):
     if isinstance(value, list):
         return [clean_nones(x) for x in value if x is not None]
     elif isinstance(value, dict):
-        return {
-            key: clean_nones(val)
-            for key, val in value.items()
-            if val is not None
-        }
+        return {key: clean_nones(val) for key, val in value.items() if val is not None}
     else:
         return value
+
 
 DISPLAY_CONTEXT = DisplayContext()
 DISPLAY_CONTEXT.commas = True
@@ -65,3 +68,18 @@ DISPLAY_CONTEXT.commas = True
 
 def print_entries(entries: Entries):
     printer.print_entries(entries, dcontext=DISPLAY_CONTEXT)
+
+
+def get_entry_type(entry: bean.Directive):
+    if isinstance(entry, bean.Open): return "Open"
+    if isinstance(entry, bean.Close): return "Close"
+    if isinstance(entry, bean.Commodity): return "Commodity"
+    if isinstance(entry, bean.Pad): return "Pad"
+    if isinstance(entry, bean.Balance): return "Balance"
+    if isinstance(entry, bean.Transaction): return "Transaction"
+    if isinstance(entry, bean.Note): return "Note"
+    if isinstance(entry, bean.Event): return "Event"
+    if isinstance(entry, bean.Query): return "Query"
+    if isinstance(entry, bean.Price): return "Price"
+    if isinstance(entry, bean.Document): return "Document"
+    if isinstance(entry, bean.Custom): return "Custom"
