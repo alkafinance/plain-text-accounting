5b8927aee0fd328a65245fca253a89fbdbf1e45c
Tony Xiao <tonyx.ca@gmail.com>
2020-08-20 09:29:58 +0800

    Allow cors request

--- main.py
+++ main.py
@@ -9,19 +9,32 @@ def handle(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600'
+    }
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        return ('', 204, headers)
+
     if 'bean_to_json' in request.path:
         data = request.get_data(as_text=True)
         json_str, *_ = bean_to_json(data)
-        return json_str, {"Content-Type": "application/json"}
+        headers["Content-Type"] = "application/json"
+        return json_str, headers
     if 'json_to_bean' in request.path:
         data = request.get_data(as_text=True)
         bean_str, *_ = json_to_bean(data)
-        return bean_str, {"Content-Type": "application/vnd+beancount"}
+        headers["Content-Type"] = "application/vnd+beancount"
+        return bean_str, headers
     return f'/json_to_bean or /bean_to_json!', 400
 
 
 if __name__ == '__main__':
     from flask import Flask, request
     app = Flask(__name__)
-    app.route('/<path>', methods=['GET', 'POST'])(lambda path: handle(request))
+    app.route('/<path>', methods=['GET', 'POST', 'OPTIONS'])(lambda path: handle(request))
     app.run(port=5552)
