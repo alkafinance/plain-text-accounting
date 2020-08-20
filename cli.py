1b8c0742f8a6de8f0799de239e0310520525e516
Tony Xiao <tonyx.ca@gmail.com>
2020-08-20 09:00:31 +0800

    Add an example of price only

--- cli.py
+++ cli.py
@@ -1,9 +1,10 @@
 from lib import bean_to_json, json_to_bean
 
-# content = open("./examples/currency-conversion.bean", mode="r", encoding="utf-8").read()
-content = open("./examples/example.beancount", mode="r", encoding="utf-8").read()
+content = open("./examples/currency-conversion.bean", mode="r", encoding="utf-8").read()
+# content = open("./examples/example.beancount", mode="r", encoding="utf-8").read()
 
-json_string, *_ = bean_to_json(content)
-bean_str, *_ = json_to_bean(json_string)
+json_str, *_ = bean_to_json(content)
+print(json_str)
 
-print(bean_str)
+# bean_str, *_ = json_to_bean(json_str)
+# print(bean_str)
